3a14fd32db66189075a018e39af60a2c2df8d77d
sashajo <sashajo@github.com>
2014-11-04 08:15:01 -0500

    Add null checks and made switchView directive robust.

--- app/scripts/controllers/switch.js
+++ app/scripts/controllers/switch.js
@@ -8,39 +8,53 @@
  * Controller of the switchviewApp
  */
 angular.module('switchviewApp')
-  .controller('SwitchCtrl', function ($scope) {
+    .controller('SwitchCtrl', function ($scope) {
 
-    $scope.mediums = [
-      'Copper',
-      'Fiber'
-    ];
+        $scope.mediums = [
+            'Copper',
+            'Fiber'
+        ];
 
-    $scope.device = {
-      n_ports: 24,
-      ports: []
-    };
+        $scope.device = null;
+//        {
+//        n_ports: 0,
+//        ports: []
+//    };
+        $scope.onClick = function(item) {
+            $scope.$apply(function() {
 
-    $scope.rebuild = function() {
-      var i, amt, base;
-      if($scope.device.n_ports === $scope.device.ports.length) {
-        return;
-      } else if($scope.device.n_ports < $scope.device.ports.length) {
-        // If the new count is lower, drop the tail
-        $scope.device.ports.splice($scope.device.n_ports, 
-                            $scope.device.ports.length-$scope.device.n_ports);
-      } else {
-        // otherwise just push to the end
-        amt = $scope.device.n_ports - $scope.device.ports.length;
-        base = $scope.device.ports.length;
-        for(i=0; i<amt; ++i) {
-          $scope.device.ports.push({
-            port_id: base+i,
-            medium: 'Copper',
-            up: true
-          });
+                console.log('Item Clicked:' +JSON.stringify(item));
+            });
+        };
+        $scope.rebuild = function () {
+            var i, amt, base;
+            if ($scope.device.n_ports === $scope.device.ports.length) {
+                return;
+            } else if ($scope.device.n_ports < $scope.device.ports.length) {
+                // If the new count is lower, drop the tail
+                $scope.device.ports.splice($scope.device.n_ports,
+                        $scope.device.ports.length - $scope.device.n_ports);
+            } else {
+                // otherwise just push to the end
+                amt = $scope.device.n_ports - $scope.device.ports.length;
+                base = $scope.device.ports.length;
+                for (i = 0; i < amt; ++i) {
+                    $scope.device.ports.push({
+                        port_id: base + i,
+                        medium: 'Copper',
+                        up: true
+                    });
+                }
+            }
         }
-      }
-    }
-    // initialize the switch with defaults
-    $scope.rebuild();
-  });
+        $scope.createDevice = function () {
+            $scope.device =
+            {
+                n_ports: 0,
+                ports: []
+            }
+            $scope.rebuild();
+        };
+        // initialize the switch with defaults
+        // $scope.rebuild();
+    });
